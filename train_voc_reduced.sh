607a52212ab00be3a3cac2657a103f6091f68d74
taokongcn <kt892790499@163.com>
2017-07-07 15:23:55 +0800

    test demo OK

--- train_voc_reduced.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-python tools/train_net.py --gpu 0 \
-  --solver models/pascalvoc/VGG16-REDUCED/solver.prototxt \
-  --imdb voc_2007_trainval+voc_2012_trainval \
-  --weights data/ImageNet_models/VGG_ILSVRC_16_layers_fc_reduced.caffemodel \
-  --batchsize 30 \
-  --iters 120000
